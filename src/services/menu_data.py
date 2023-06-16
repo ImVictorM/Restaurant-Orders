42b3df1aaf98ca2baf90e150bbb5cfb280bf1968
Victor Mendes <victorutdsim7@gmail.com>
2023-06-16 18:03:40 -0300

    feat: implementa parcialmente o construtor da classe MenuData

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,19 @@
-# Req 3 oii
+import pandas as pd
+from models.dish import Dish
+from models.ingredient import Ingredient
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        df = pd.read_csv(source_path)
+        self.dishes = set()
+
+        grouped_df = (
+            df.groupby(["dish", "price"])
+            .apply(lambda df: list(zip(df["ingredient"], df["recipe_amount"])))
+        )
+
+        print(grouped_df)
+
+
+MenuData("data/menu_base_data.csv")
