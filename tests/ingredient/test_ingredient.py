b742748a08c12ccd91a72620bf11e9e554fcc765
Victor Mendes <victorutdsim7@gmail.com>
2023-06-16 15:02:01 -0300

    test: classe Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,26 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient
 
 
-# Req 1
 def test_ingredient():
-    pass
+    bacon = Ingredient("bacon")
+    farinha = Ingredient("farinha")
+
+    # test_ingredient_name
+    assert bacon.name == "bacon"
+    assert farinha.name == "farinha"
+
+    # test_invalid_ingredient
+    invalid_ingredient = Ingredient("invalid_ingredient")
+    assert invalid_ingredient.restrictions == set()
+
+    # test_method__eq__
+    assert farinha != bacon
+    assert bacon == bacon
+
+    # test_method__repr__
+    assert repr(farinha) == "Ingredient('farinha')"
+    assert repr(bacon) == "Ingredient('bacon')"
+
+    # test_method__hash__
+    assert hash(Ingredient("bacon")) == hash(Ingredient("bacon"))
+    assert hash(Ingredient("bacon")) != hash(Ingredient("farinha"))
