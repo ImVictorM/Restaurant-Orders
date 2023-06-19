23043b894f1a4ed6a7e6e297ed44996c5e07cf9f
Victor Mendes <victorutdsim7@gmail.com>
2023-06-19 20:31:19 -0300

    refactor: organiza testes

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,26 +1,37 @@
 from src.models.ingredient import Ingredient
+import pytest
 
 
-def test_ingredient():
-    bacon = Ingredient("bacon")
-    farinha = Ingredient("farinha")
+@pytest.fixture
+def bacon():
+    return Ingredient("bacon")
 
-    # test_ingredient_name
+
+@pytest.fixture
+def farinha():
+    return Ingredient("farinha")
+
+
+def test_ingredient_name(bacon, farinha):
     assert bacon.name == "bacon"
     assert farinha.name == "farinha"
 
-    # test_invalid_ingredient_has_no_restrictions
-    invalid_ingredient = Ingredient("invalid_ingredient")
-    assert invalid_ingredient.restrictions == set()
 
-    # test_method__eq__
+def test_ingredient_equality(bacon, farinha):
     assert farinha != bacon
     assert bacon == bacon
 
-    # test_method__repr__
+
+def test_ingredient_representation(bacon, farinha):
     assert repr(farinha) == "Ingredient('farinha')"
     assert repr(bacon) == "Ingredient('bacon')"
 
-    # test_method__hash__
-    assert hash(Ingredient("bacon")) == hash(Ingredient("bacon"))
-    assert hash(Ingredient("bacon")) != hash(Ingredient("farinha"))
+
+def test_ingredient_hashing(bacon, farinha):
+    assert hash(bacon) == hash(bacon)
+    assert hash(bacon) != hash(farinha)
+
+
+def test_invalid_ingredient_has_no_restrictions():
+    invalid_ingredient = Ingredient("invalid_ingredient")
+    assert invalid_ingredient.restrictions == set()
